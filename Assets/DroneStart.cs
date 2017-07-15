e6016a0b72e678d86706fbf47a42829ff6d2b48e
Jonathan Lee <jsalinlee@gmail.com>
2017-07-15 03:33:58 -0700

    Configured drones to deploy out in front of player.

--- Assets/DroneStart.cs
+++ Assets/DroneStart.cs
@@ -20,20 +20,36 @@ public class DroneStart : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		angleOfRotation += speed * Time.deltaTime;
-		float x = Mathf.Cos(angleOfRotation) * radius + OrbitTarget.transform.position.x;
-		float z = Mathf.Sin(angleOfRotation) * radius + OrbitTarget.transform.position.z;
-
 		if (spawnAnimation) {
-			StartCoroutine(Spawn(x, z));
+			Quaternion angle = Quaternion.LookRotation(OrbitTarget.transform.forward);
+			Quaternion target = Quaternion.LookRotation(Vector3.right);
+
+			var forwardA = angle * Vector3.forward;
+			var forwardB = target * Vector3.forward;
+
+			var angleA = Mathf.Atan2(forwardA.x, forwardA.z);
+			var angleB = Mathf.Atan2(forwardB.x, forwardB.z);
+
+			angleOfRotation = Mathf.DeltaAngle(angleA, angleB);
+
+			float x = Mathf.Cos(angleOfRotation) * radius + OrbitTarget.transform.position.x;
+			float z = Mathf.Sin(angleOfRotation) * radius + OrbitTarget.transform.position.z;
+			float y = OrbitTarget.transform.position.y + 1;
+			StartCoroutine(Spawn(x, y, z));
+		} else {
+	//		transform.GetComponent<Rigidbody>().useGravity = true;
+			angleOfRotation += speed * Time.deltaTime;
+			float x = Mathf.Cos(angleOfRotation) * radius + OrbitTarget.transform.position.x;
+			float z = Mathf.Sin(angleOfRotation) * radius + OrbitTarget.transform.position.z;
+			float y = OrbitTarget.transform.position.y + 1;
+	        OrbitAround(x, y, z);
 		}
-//		transform.GetComponent<Rigidbody>().useGravity = true;
-        OrbitAround(x, z);
 	}
 
-	IEnumerator Spawn (float x, float z) {
-		position = new Vector3(x, OrbitTarget.transform.position.y + 1, z);
+	IEnumerator Spawn (float x, float y, float z) {
+		position = new Vector3(x, y, z);
 		float timeSinceStarted = 0f;
+		spawnAnimation = false;
 		while(true) {
 			timeSinceStarted += Time.deltaTime;
 			transform.position = Vector3.Lerp(transform.position, position, timeSinceStarted / 20);
@@ -44,9 +60,10 @@ public class DroneStart : MonoBehaviour {
 		}
 	}
 
-    void OrbitAround(float x, float z) {
-		position = new Vector3(x, OrbitTarget.transform.position.y + 1, z);
-		transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime);
+	void OrbitAround(float x, float y, float z) {
+//		print(angleOfRotation);
+		position = new Vector3(x, y, z);
+		transform.position = Vector3.Lerp(transform.position, position, 10 * Time.deltaTime);
 
 //		transform.position = new Vector3(OrbitTarget.transform.position.x + 2, OrbitTarget.transform.position.y, OrbitTarget.transform.position.z + 2);
 //		transform.RotateAround(OrbitTarget.transform.position, new Vector3(0, 1, 0), 90 * Time.deltaTime);
