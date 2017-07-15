ad061247bfecf4f85b6f370554bcbd950c366e5e
DEXTER-PC-MKII\Dexter <nicimoc@gmail.com>
2017-07-15 03:33:29 -0700

    Added robo-player model and simple zoom limiter, also added some dunes and baked them into the navmesh

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -15,15 +15,21 @@ public class CameraMovement : MonoBehaviour {
 	// Update is called once per frame
 
     void Zoom(){
-        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
+            if(ydistance <= -6)
+            { 
             ydistance = ydistance+2;
             zdistance = zdistance-1;
+            }
         }
         else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
         {
-            ydistance = ydistance-2;
-            zdistance = zdistance+1;
+            if(ydistance >= -10)
+            {
+            ydistance = ydistance - 2;
+            zdistance = zdistance + 1;
+            }
         }
     }
     void Update()
