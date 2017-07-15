ea74821575f27249d9dd572a9286549a364217cd
DEXTER-PC-MKII\Dexter <nicimoc@gmail.com>
2017-07-14 18:09:34 -0700

    Adding shit in for the first time

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    
+    public float xdistance;
+    public float ydistance;
+    public float zdistance;
+    public Player target;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+    void Zoom(){
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            ydistance = ydistance+2;
+            zdistance = zdistance-1;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            ydistance = ydistance-2;
+            zdistance = zdistance+1;
+        }
+    }
+    void Update()
+    {
+        Zoom();
+        transform.position = new Vector3(target.transform.position.x - xdistance, target.transform.position.y - ydistance, target.transform.position.z - zdistance);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent navAgent;
+    public GameObject projectile;
+
+    // Use this for initialization
+    void throwDrone()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 150);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.up * 450);
+        }
+    }
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void clickToMove()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) // 0 is Left-Click, 1 is Right-Click
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                navAgent.SetDestination(hit.point);
+            }
+        }
+    }
+    void Update()
+    {
+        clickToMove();
+        throwDrone();
+    }
+}
